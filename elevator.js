9ee75878c3b9052afe899fa1f5526b08e760780d
Shebo <shebo6@gmail.com>
2021-12-07 23:33:19 +0200

    1

--- elevator.js
+++ elevator.js
@@ -22,8 +22,7 @@ function Elevator(speedFloorsPerSec, floorCount, floorHeight, maxUsers, startFlo
     elevator.currentFloor = 0;
     elevator.previousTruncFutureFloorIfStopped = 0;
 
-
-    elevator.buttonStates = _.map(_([0]).concat(_.range(startFloor, floorCount)), function(e, i){ return false; });
+    elevator.buttonStates = _.map([0].concat(_.range(startFloor, floorCount)), function(e, i){ return false; });
     elevator.moveCount = 0;
     elevator.removed = false;
     elevator.userSlots = _.map(_.range(elevator.maxUsers), function(user, i) {
